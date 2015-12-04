82cdc11abeba57dac368def66c247ad20df0d5d7
HelloWorld017 <deu07115@gmail.com>
2015-12-04 23:35:49 +0900

    Added support for custom multidimensional config

--- config/index.js
+++ config/index.js
@@ -1,18 +1,3 @@
 var env = process.env.NODE_ENV || 'development';
 
-module.exports = cherryPick(require('./config.' + env), require('./config.custom.js'));
-
-
-function cherryPick(baseArray, overridingArray){
-	var finalArray = {};
-
-	Object.keys(baseArray).forEach(function(v){
-		finalArray[v] = baseArray[v];
-	});
-
-	Object.keys(overridingArray).forEach(function(v){
-		finalArray[v] = overridingArray[v];
-	});
-
-	return finalArray;
-}
+module.exports = require('object-merge')(require('./config.' + env), require('./config.custom.js'));
